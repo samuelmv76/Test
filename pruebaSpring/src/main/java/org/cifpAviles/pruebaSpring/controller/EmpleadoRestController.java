5f4c37f7b4f3754126db774d42a083372de22397
Samuel Martos <samuelmv04@gmail.com>
2025-11-17 19:49:22 +0100

    crud

--- pruebaSpring/src/main/java/org/cifpAviles/pruebaSpring/controller/EmpleadoRestController.java
+++ pruebaSpring/src/main/java/org/cifpAviles/pruebaSpring/controller/EmpleadoRestController.java
@@ -1,11 +1,16 @@
 package org.cifpAviles.pruebaSpring.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.cifpAviles.pruebaSpring.entities.Empleado;
 import org.cifpAviles.pruebaSpring.services.EmpleadoService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -17,6 +22,7 @@ public class EmpleadoRestController {
 	
 	public EmpleadoRestController(EmpleadoService empleadoService) {
 		this.empleadoService = empleadoService;
+
 	}
 	
 	@GetMapping("info_empleado")
@@ -25,5 +31,23 @@ public class EmpleadoRestController {
 		return empleadoService.findAll();
 	}
 	
+	@PostMapping("add_empleado")
+	public void crearEmpleado(@RequestBody Empleado empleado) {
+		empleadoService.insert(empleado);
+		
+	}
+	
+	@GetMapping("get_empleado/{id}")
+	@ResponseBody
+	public Optional<Empleado> obtenerEmpleadoPorId(@PathVariable Long id){
+		return empleadoService.findById(id);
+	}
+	
+	@DeleteMapping("delete_empleado/{id}")
+	public void eliminarEmpleadoPorId(@PathVariable Long id) {
+		Empleado emp = new Empleado();
+		emp.setId(id);
+		empleadoService.borrar(emp);
+	}
 	
 }
